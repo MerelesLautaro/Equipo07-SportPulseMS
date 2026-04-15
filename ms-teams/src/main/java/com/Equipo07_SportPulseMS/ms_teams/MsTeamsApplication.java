cc48cbc4c9a77af9d616e67b727220562d4009e1
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-04-15 16:29:06 -0600

    chore(ms-teams): enable OpenFeign clients in the application

--- ms-teams/src/main/java/com/Equipo07_SportPulseMS/ms_teams/MsTeamsApplication.java
+++ ms-teams/src/main/java/com/Equipo07_SportPulseMS/ms_teams/MsTeamsApplication.java
@@ -2,12 +2,12 @@ package com.Equipo07_SportPulseMS.ms_teams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsTeamsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsTeamsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsTeamsApplication.class, args);
+    }
 }
