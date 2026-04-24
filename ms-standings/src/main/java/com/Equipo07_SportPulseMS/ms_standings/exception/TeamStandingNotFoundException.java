671a36c6c5833289317f9297240f5c795d8e11f8
Joaquin Osorio <osoriojoaquin785@gmail.com>
2026-04-23 23:36:53 -0300

    feat(ms-standings): add hu-18 dto and not-found exception

---
+++ ms-standings/src/main/java/com/Equipo07_SportPulseMS/ms_standings/exception/TeamStandingNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Equipo07_SportPulseMS.ms_standings.exception;
+
+public class TeamStandingNotFoundException extends RuntimeException {
+
+    public TeamStandingNotFoundException(String message) {
+        super(message);
+    }
+}
